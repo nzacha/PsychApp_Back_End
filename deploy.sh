d93188ee790955e60c945477f867697f5961516d
nzacha <zicolas3@gmail.com>
2020-09-30 10:15:44 +0300

    added mysql libraries

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 export DB_USER=psychadmin
 export DB_HOST=localhost
 export DB_NAME=psychapp
+
 export DB_PASSWORD='psych'
+//export DB_PASSWORD='password'
 export DB_PORT=5432
+//export DB_PORT=3306
+
 export PORT=5050
 export HOST=localhost
 
